8dd954c8ce1105c72aad19d5bbcc35cacfbeb372
SharyckR <120688805+SharyckR@users.noreply.github.com>
2023-10-05 16:09:19 -0500

    Update person.py
    Se añadió el historial de educación y se conecto la clase con el Mediador.

--- logic/person.py
+++ logic/person.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from logic.address import Address
+from logic.education_history import EducationHistory
 
 
 class Person(BaseModel):
@@ -14,6 +15,8 @@ class Person(BaseModel):
             last_name (str): Last name of person.
             phone (int): Phone of person.
             address (object): Address of person.
+            education_history (object): Educational history of the person.
+            mediator (object): Mediator for managing interactions.
 
         Methods:
             __str__(): Returns a string representation of a person.
@@ -27,6 +30,12 @@ class Person(BaseModel):
     last_name: str = "LastName"
     phone: int = 0
     address: object = Address
+    education_history: object = EducationHistory
+    mediator: object = None
+
+    def __init__(self, mediator=None, **data):
+        super().__init__(**data)
+        self.mediator = mediator
 
     def __eq__(self, another_person):
         """ Returns bool of equality of person objects.
@@ -40,5 +49,6 @@ class Person(BaseModel):
         :returns: string person
         :rtype: str
         """
-        return 'Id Person: {0}, Type Id: {1}, Dni: {2}, Full name: {3} {4}, Phone: {5}, Address: {6}'.format(
-            self.id_person, self.type_id, self.dni, self.name, self.last_name, self.phone, self.address)
+        return 'Id Person: {0}, Type Id: {1}, Dni: {2}, Full name: {3} {4}, Phone: {5}, Address: {6}, ' \
+               'Education History: {7}'.format(self.id_person, self.type_id, self.dni, self.name, self.last_name,
+                                               self.phone, self.address, self.education_history)
