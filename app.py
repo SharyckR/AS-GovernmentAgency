28c08c7a7857038c56517c98af4a222aa1eb4b98
SharyckR <120688805+SharyckR@users.noreply.github.com>
2023-11-10 14:53:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from routers import (educational_agency_router, health_agency_router, legal_agency_router, person_router,
                      transport_agency_router, educational_history_router, health_history_router, case_history_router,
                      fine_history_router, vehicle_history_router)
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(educational_agency_router.router)
 app.include_router(health_agency_router.router)
